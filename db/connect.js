850649109dd4a85606bb4dc321db3e2b873f7744
rishi <prajapatirishi2005@gmail.com>
2025-12-18 16:04:00 +0530

    a

--- db/connect.js
+++ db/connect.js
@@ -5,19 +5,28 @@ dotenv.config();
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
     port: parseInt(process.env.DB_PORT) || 3306,
     dialect: process.env.DB_DIALECT || "mysql",
     timezone: "+05:30",
+    logging: false,
+
+    // SSL 
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+
     pool: {
       max: parseInt(process.env.DB_POOL_MAX) || 5,
       min: parseInt(process.env.DB_POOL_MIN) || 0,
       acquire: parseInt(process.env.DB_POOL_ACQUIRE) || 30000,
       idle: parseInt(process.env.DB_POOL_IDLE) || 10000,
     },
-    logging: false,
   }
 );
 
@@ -28,7 +37,7 @@ const sequelize = new Sequelize(
   } catch (err) {
     console.error("❌ Database connection failed!");
     console.error("Reason:", err.message);
-    process.exit(1); 
+    process.exit(1);
   }
 })();
 
