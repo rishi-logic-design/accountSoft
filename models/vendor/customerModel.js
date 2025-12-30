923e4f99efb80d0080360f51543041d019d15048
rishi <prajapatirishi2005@gmail.com>
2025-12-30 16:52:50 +0530

    update customer model

--- models/vendor/customerModel.js
+++ models/vendor/customerModel.js
@@ -7,52 +7,54 @@ module.exports = (sequelize, DataTypes) => {
         primaryKey: true,
         autoIncrement: true,
       },
-      customerName: { 
-        type: DataTypes.STRING, 
-        allowNull: false 
+      customerName: {
+        type: DataTypes.STRING,
+        allowNull: false,
       },
-      businessName: { 
-        type: DataTypes.STRING, 
-        allowNull: true 
+      businessName: {
+        type: DataTypes.STRING,
+        allowNull: true,
       },
-      mobileNumber: { 
-        type: DataTypes.STRING, 
-        allowNull: false 
+      mobileNumber: {
+        type: DataTypes.STRING,
+        allowNull: false,
       },
-      email: { 
-        type: DataTypes.STRING, 
+      email: {
+        type: DataTypes.STRING,
         allowNull: true,
         validate: {
-          isEmail: true
-        }
+          isEmail: true,
+        },
       },
-      gstNumber: { 
-        type: DataTypes.STRING, 
-        allowNull: true 
+      gstNumber: {
+        type: DataTypes.STRING,
+        allowNull: true,
       },
-      homeAddress: { 
-        type: DataTypes.TEXT, 
-        allowNull: true 
+      homeAddress: {
+        type: DataTypes.JSON,
+        allowNull: true,
+        defaultValue: null,
       },
-      officeAddress: { 
-        type: DataTypes.TEXT, 
-        allowNull: true 
+      officeAddress: {
+        type: DataTypes.JSON,
+        allowNull: true,
+        defaultValue: null,
       },
-      customerImage: { 
-        type: DataTypes.STRING, 
-        allowNull: true 
+      customerImage: {
+        type: DataTypes.STRING,
+        allowNull: true,
       },
-      aadharNumber: { 
-        type: DataTypes.STRING, 
-        allowNull: true 
+      aadharNumber: {
+        type: DataTypes.STRING,
+        allowNull: true,
       },
-      pricePerProduct: { 
-        type: DataTypes.DECIMAL(10, 2), 
-        allowNull: true 
+      pricePerProduct: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: true,
       },
-      createdBy: { 
-        type: DataTypes.INTEGER.UNSIGNED, 
-        allowNull: false 
+      createdBy: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false,
       },
     },
     {
