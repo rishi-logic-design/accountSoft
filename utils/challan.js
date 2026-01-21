e8c039b9c2688481c7eb4aa9834f6de57f1c92ba
rishi <prajapatirishi2005@gmail.com>
2026-01-21 17:11:15 +0530

    challan

--- utils/challan.js
+++ utils/challan.js
@@ -1,3 +1,5 @@
+const { Op } = require("sequelize");
+
 const pad = (num, size) => {
   let s = String(num);
   while (s.length < size) s = "0" + s;
@@ -11,11 +13,26 @@ exports.generateChallanNumber = async (ChallanModel, transaction = null) => {
   const d = pad(today.getDate(), 2);
   const prefix = `CH-${y}${m}${d}`;
 
-  const where = {
-    challanDate: `${y}-${m}-${d}`,
-  };
+  // Find the last challan number with this prefix
+  const lastChallan = await ChallanModel.findOne({
+    where: {
+      challanNumber: {
+        [Op.like]: `${prefix}%`,
+      },
+    },
+    order: [["challanNumber", "DESC"]],
+    transaction,
+    lock: transaction ? transaction.LOCK.UPDATE : undefined,
+  });
 
-  const count = await ChallanModel.count({ where, transaction });
-  const seq = pad(count + 1, 4);
+  let sequence = 1;
+
+  if (lastChallan && lastChallan.challanNumber) {
+    // Extract the sequence number from the last challan number
+    const lastSequence = lastChallan.challanNumber.split("-").pop();
+    sequence = parseInt(lastSequence, 10) + 1;
+  }
+
+  const seq = pad(sequence, 4);
   return `${prefix}-${seq}`;
 };
