3d9dd511ab0e2e58be3c2778a769e597c6efa0b0
rishi <prajapatirishi2005@gmail.com>
2026-01-30 14:59:01 +0530

    space delete

--- routes/Customer/customerProfileRoutes.js
+++ routes/Customer/customerProfileRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/authMiddleware");
-const controller = require("../../controllers/customer/customerProfileController    ");
+const controller = require("../../controllers/customer/customerProfileController");
 
 router.get("/", auth, controller.getMyProfile);
 router.put("/", auth, controller.updateMyProfile);
