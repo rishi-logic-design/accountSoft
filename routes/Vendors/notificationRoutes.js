d9e2c83975c42d84135f4d28d840aa6f0c3c5249
rishi <prajapatirishi2005@gmail.com>
2026-02-05 09:54:55 +0530

    sdsadas

--- routes/Vendors/notificationRoutes.js
+++ routes/Vendors/notificationRoutes.js
@@ -4,9 +4,8 @@ const controller = require("../../controllers/vendor/notificationController");
 const auth = require("../../middleware/authMiddleware");
 const customerAuth = require("../../middleware/customerAuthMiddleware");
 
-router.get("/", auth, customerAuth, controller.getMyNotifications);
-router.put("/:notificationId/read", auth, customerAuth, controller.markAsRead);
-router.put("/read-all", auth, customerAuth, controller.markAllRead);
+router.get("/", auth, controller.getMyNotifications);
+router.put("/:notificationId/read", auth, controller.markAsRead);
+router.put("/read-all", auth, controller.markAllRead);
 
 module.exports = router;
- 
